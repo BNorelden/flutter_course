ed302395b79d8cf61daae64a95cbc6c782ea3f67
Bilal <noreldenb1@montclair.edu>
2023-10-31 00:37:23 -0400

    Flutter Widget example Lifecycle in prod_m n Prods

--- lib/products_manager.dart
+++ lib/products_manager.dart
@@ -3,19 +3,40 @@ import 'package:flutter/material.dart';
 import './products.dart';
 
 class ProductManager extends StatefulWidget {
+  final String startingProduct; //how to get this from point a
+
+  ProductManager(this.startingProduct) {
+    print('[ProductsManager Widget] Constructor');
+  }
+
   @override
   State<StatefulWidget> createState() {
-    // TODO: implement createState
+    print('[ProductsManager Widget] createState()');
     return _ProductManagerState();
   }
 }
 
 class _ProductManagerState extends State<ProductManager> {
-  List<String> _products = ['Food Tester'];
+  List<String> _products = [];
+
+  @override
+  void initState() {
+    print('[ProductsManager State] initState()');
+
+    _products.add(widget.startingProduct); //to point b using widget
+    super.initState();
+  }
+
+  @override
+  void didUpdateWidget(covariant ProductManager oldWidget) {
+    print(
+        '[ProductsManager State] didUpdateWidget()'); //didn't show up in the debug console :(
+    super.didUpdateWidget(oldWidget);
+  }
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
+    print('[ProductsManager State] build()');
     return Column(
       children: [
         Container(
